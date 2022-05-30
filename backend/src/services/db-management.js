b2cd38f1d48652b2b6ecfe2f4757a1ae3608ebe3
Razvan Roatis <razvanroatis91@gmail.com>
2022-05-30 23:17:00 +0200

    Fully working (dumb) DB

--- backend/src/services/db-management.js
+++ backend/src/services/db-management.js
@@ -3,12 +3,9 @@ const fs = require('fs');
 const PATH_STORAGE = './tmp/';
 const PATH_DB = PATH_STORAGE + 'photos.json';
 
-// A stupidly painful way of checking if DB exists
-try {
-  if (fs.existsSync(path)) {
-  }
-} catch (err) {
-  console.error(err)
+// A painful, stupid way of checking if DB exists
+if (!fs.existsSync(PATH_DB)) {
+  fs.writeFileSync(PATH_DB, JSON.stringify({}));
 }
 
 let _photos = JSON.parse(fs.readFileSync(PATH_DB));
@@ -16,15 +13,14 @@ let _photos = JSON.parse(fs.readFileSync(PATH_DB));
 let getUnsavedPhotoIdList = (userId, photoIdList) => {
   if (!(userId in _photos)) {
     _photos[userId] = {
-      photoIdSet: new Set(),
-      photos: []
+      photoIdSet: []
     };
   }
 
   let unsavedPhotos = [];
   for (let i = 0; i < photoIdList.length; i++) {
     let photoId = photoIdList[i];
-    if (!_photos[userId].photoIdSet.has(photoId)) {
+    if (!_photos[userId].photoIdSet.includes(photoId)) {
       unsavedPhotos.push(photoId);
     }
   }
@@ -34,17 +30,17 @@ let getUnsavedPhotoIdList = (userId, photoIdList) => {
 let savePhotos = (userId, photos) => {
   if (!(userId in _photos)) {
     _photos[userId] = {
-      photoIdSet: new Set(),
-      photos: []
+      photoIdSet: []
     };
   }
 
   photos.forEach((photo) => {
-    _photos[userId].photoIdSet.add(photo.photoId);
-    fs.writeFile(PATH_TO_STORAGE + photo.photoId, photo.photoEncoding.data, 'base64', function (err) {
+    _photos[userId].photoIdSet.push(photo.photoId);
+    fs.writeFile(PATH_STORAGE + photo.photoId, photo.photoEncoding.data, 'base64', function (err) {
       console.log(err);
     });
   });
+  fs.writeFileSync(PATH_DB, JSON.stringify(_photos));
 };
 
 module.exports = {
